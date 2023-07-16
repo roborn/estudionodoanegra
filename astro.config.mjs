aaaf8665372183d06e1b14cf4ff75db9c9339a63
Roberto Ornelas <rob@front.no>
2023-07-16 19:06:28 +0100

    Github deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://estudionodoanegra.com",
+  site: "https://estudionodoanegra.github.io",
   integrations: [
     tailwind(),
     image({
